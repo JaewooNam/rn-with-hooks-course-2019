5411fc56624309aba065fa4ac6ab7fc5ff40db17
JaewooNam <namssabu12@gmail.com>
2019-09-15 13:59:53 +0900

    Error handling

--- food/src/api/yelp.js
+++ food/src/api/yelp.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
     Authorization:
-      'Bearer Dtct2Wxx5abgmtMGA-cruOZSyId6FiBJbq-1m2K-GF-3JdYvhz3_2xbolUxdYNDRIEgZ728I4rs46R27b8WvGnAD6E8ZGlq8iq_QQVY9jHwLwysbb1wTzMSWlil9XXYx'
+      'BearerDtct2Wxx5abgmtMGA-cruOZSyId6FiBJbq-1m2K-GF-3JdYvhz3_2xbolUxdYNDRIEgZ728I4rs46R27b8WvGnAD6E8ZGlq8iq_QQVY9jHwLwysbb1wTzMSWlil9XXYx'
   }
 })
