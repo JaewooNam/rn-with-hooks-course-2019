147d9acd55d79606dee5966fd315a5f50ba3ca70
JaewooNam <namssabu12@gmail.com>
2019-09-22 21:18:50 +0900

    save user db

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,12 +1,20 @@
+require('./models/User')
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const authRoutes = require('./routes/authRoutes')
+
 const app = express()
+
+app.use(bodyParser.json())
+app.use(authRoutes)
 
 const mongoUri = 'mongodb+srv://admin:ska37589@cluster0-2siig.mongodb.net/test?retryWrites=true&w=majority'
 mongoose.connect(mongoUri, {
   useNewUrlParser: true,
   useCreateIndex: true
 })
+
 mongoose.connection.on('connected', () => {
   console.log('Connected on mongo instance')
 })
