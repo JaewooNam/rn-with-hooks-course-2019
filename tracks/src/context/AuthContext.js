d02526ec7b123102a75572968f15b795ab5cfbba
JaewooNam <namssabu12@gmail.com>
2019-09-23 23:45:05 +0900

    save

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -1,29 +1,45 @@
+import { AsyncStorage } from 'react-native'
 import createDataContext from './createDataContext'
 import trackerApi from '../api/tracker'
 import tracker from '../api/tracker';
+import { navigate } from '../navigationRef'
 
 const authReducer = (state, action) => {
   switch (action.type) {
     case 'add_error':
       return { ...state, errorMessage: action.payload }
+    case 'signup':
+      return { errorMessage: '', token: action.payload }
     default:
       return state
   }
 }
 
-const signup = (dispatch) => {
-  return async ({ email, password }) => {
-    // make api request to sign up with that email and password
-    // if we sign up, modify our state, and say that we are authenticated
-    // if signing up fails, we probably need to reflext an error message
-    try {
-      const response = await trackerApi.post('/signup', { email, password })
-      console.log(response.data)
-    } catch (err) {
-      dispatch({ type: 'add_error', payload: 'Something went wrong with sign up' })
-    }
+// EXAMPLE ARROW FUNCTION
+// const add = (a,b)=> {
+//   return a + b
+// }
+
+// const add = (a,b)=> a + b
+
+const signup = (dispatch) => async ({ email, password }) => {
+  // make api request to sign up with that email and password
+  // if we sign up, modify our state, and say that we are authenticated
+  // if signing up fails, we probably need to reflext an error message
+  try {
+    const response = await trackerApi.post('/signup', { email, password })
+    // console.log(response.data) // token
+    await AsyncStorage.setItem('token', response.data.token) // for auto login
+    // await AsyncStorage.getItem('token')
+    dispatch({ type: 'signup', payload: response.data.token })
+
+    // navigate to main flow
+    navigate('TrackList')
+  } catch (err) {
+    dispatch({ type: 'add_error', payload: 'Something went wrong with sign up' })
   }
 }
+
 
 const signin = (dispatch) => {
   return ({ email, password }) => {
@@ -41,6 +57,6 @@ const signout = (dispatch) => {
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  { signin, signout, signup},
-  { isSignedIn: false, errorMessage: '' } // Initial State
+  { signin, signout, signup },
+  { token: null, errorMessage: '' } // Initial State
 )
