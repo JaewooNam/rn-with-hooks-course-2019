7acb94df1072ec2a9e07e39cf496db4074899531
JaewooNam <namssabu12@gmail.com>
2019-09-23 11:16:26 +0900

    create track routes

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,14 +1,17 @@
 require('./models/User')
+require('./models/Track')
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const authRoutes = require('./routes/authRoutes')
 const requireAuth = require('./middlewares/requireAuth')
+const trackRoutes = require('./routes/trackRoutes')
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(authRoutes)
+app.use(trackRoutes)
 
 const mongoUri = 'mongodb+srv://admin:ska37589@cluster0-2siig.mongodb.net/test?retryWrites=true&w=majority'
 mongoose.connect(mongoUri, {
